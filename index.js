deb38b8c12cacc24c6ac621df1c9159ecc1e6230
Michael Sobolev <michaelsobolev@Michaels-MacBook-Pro.local>
2017-03-10 15:55:36 -0500

    cors

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // Set our api routes
 app.use('/api', api);
